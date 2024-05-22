bb29b0b14a129020327cd8ee4b601beef587e923
void-hoge <muginoda@gmail.com>
2024-05-22 10:54:00 +0900

    experiment

---
+++ experiment.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import time
+
+from optimizer import Optimizer, InfeasibleProblemError
+import problem
+
+def presolve(probname, probclass):
+    for name in ['default', 'noda_mt']:
+        with open(f'gpclist/{name}.json', 'r') as f:
+            gpclist = json.loads(f.read())
+        for size in range(16, 33):
+            begin = time.time()
+            for stage in range(10):
+                prob = probclass(size, 2, stage, gpclist)
+                try:
+                    opt = Optimizer(prob.get_dict(), objective=None)
+                    sol = opt.solve(timelimit=10)
+                    end = time.time()
+                    with open(f'results/presolve_{name}_{probname}_{size}_{stage}.json', 'w') as f:
+                        print(
+                            json.dumps({'problem': prob.get_dict(), 'solution': sol, 'time': end - begin}),
+                            file=f
+                        )
+                        break
+                except InfeasibleProblemError:
+                    pass
+
+if __name__ == '__main__':
+    presolve(probname = 'multiplier', probclass = problem.multiplier.Multiplier)
+    presolve(probname = 'square', probclass = problem.square.Square)
